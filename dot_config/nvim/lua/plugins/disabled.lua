a5d0c84d5e78a64dd5e5931f5d70e2727c773e49
Théo DEPELSEMACKER <theo@audi-on.com>
2024-07-13 14:22:37 +0200

    Add .config/nvim/LICENSE Add .config/nvim/README.md Add .config/nvim/.gitignore Add .config/nvim/.neoconf.json Add .config/nvim/init.lua Add .config/nvim/lazy-lock.json Add .config/nvim/lazyvim.json Add .config/nvim/lua/config/autocmds.lua Add .config/nvim/lua/config/keymaps.lua Add .config/nvim/lua/config/lazy.lua Add .config/nvim/lua/config/options.lua Add .config/nvim/lua/plugins/colorscheme.lua Add .config/nvim/lua/plugins/disabled.lua Add .config/nvim/lua/plugins/example.lua Add .config/nvim/lua/plugins/plugins.lua Add .config/nvim/stylua.toml

---
+++ dot_config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,9 @@
+return {
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  {
+    "nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = false },
+    },
+  },
+}
---
+++ dot_config/nvim/lua/plugins/plugins.lua
@@ -0,0 +1,43 @@
+return {
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<space>fe", ":Telescope file_browser path=%:p:h select_buffer=true<CR>" },
+    },
+  },
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_fmt = {
+        html = { "prettierd", "prettier" },
+        javascript = { "prettierd", "prettier" },
+        json = { "prettierd", "prettier" },
+        markdown = { "prettierd", "prettier" },
+        scss = { "prettierd", "prettier" },
+        typescript = { "prettierd", "prettier" },
+        yaml = { "prettierd", "prettier" },
+        javascriptreact = { "prettierd", "prettier" },
+        typescriptreact = { "prettierd", "prettier" },
+        css = { "prettierd", "prettier" },
+      },
+    },
+  },
+}
