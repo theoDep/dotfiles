69c8b535f13c28e0b4df2a6636f6218d632ffe8d
Théo DEPELSEMACKER <theo@audi-on.com>
2024-07-20 16:43:57 +0200

    Update .config/nvim/lazy-lock.json Update .config/nvim/lazyvim.json Update .config/nvim/lua/plugins/plugins.lua

--- dot_config/nvim/lua/plugins/plugins.lua
+++ dot_config/nvim/lua/plugins/plugins.lua
@@ -40,4 +40,13 @@ return {
       },
     },
   },
+  {
+    "nvim-neotest/neotest-jest",
+  },
+  {
+    "nvim-neotest/neotest",
+    opts = {
+      adapters = { "neotest-jest" },
+    },
+  },
 }
